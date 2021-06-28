7a58fe4a528dca0d64aab3e01306a41f7e32a93f
Nattan <nattan.moreira0803@gmail.com>
2021-06-28 17:10:38 -0300

    Sequência da segunda tela

--- UnitCadastromobile.pas
+++ UnitCadastromobile.pas
@@ -5,13 +5,19 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects,
-  System.ImageList, FMX.ImgList, FMX.StdCtrls, FMX.Controls.Presentation;
+  System.ImageList, FMX.ImgList, FMX.StdCtrls, FMX.Controls.Presentation,
+  FMX.Layouts, FMX.ListBox;
 
 type
   TfrmCadastroMobile = class(TForm)
     Panel1: TPanel;
-    btnInserir: TSpeedButton;
-    Image1: TImage;
+    btnInserir: TButton;
+    ImageList1: TImageList;
+    StyleBook1: TStyleBook;
+    btnDeletar: TButton;
+    btnSair: TButton;
+    Layout1: TLayout;
+    ListBox1: TListBox;
   private
     { Private declarations }
   public
